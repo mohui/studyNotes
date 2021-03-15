98f8eae98632192fcd49f4d187300ad3035f3f9b
王贺辉 <wanghehui@bjknrt.com>
2021-03-15 11:01:22 +0800

    删除示例代码

--- src/test.js
+++ src/test.js
@@ -19,3 +19,19 @@ const Hello = require('./exportTest');
 const hello = new Hello();
 hello.setName('魔辉');
 hello.sayHello();
+
+
+
+// //设置主机名
+// const hostName = '127.0.0.1';
+// //设置端口
+// const port = 1011;
+// //创建服务
+// const server = http.createServer(function(req,res){
+//     res.setHeader('Content-Type','text/plain');
+//     res.end("hello nodejs");
+//
+// });
+// server.listen(port,hostName,function(){
+//     console.log(`服务器运行在http://${hostName}:${port}`);
+// });
